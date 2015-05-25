d6b19be51ee93e3f4685edb6b81330b8da1322ea
futonaka <futonaka@lassic.co.jp>
2015-05-25 11:06:22 +0900

    Add settings for captureful_formatter.

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -9,3 +9,11 @@ Capybara.default_driver    = :poltergeist
 Capybara.javascript_driver = :poltergeist
 
 Dir.glob("spec/**/*_steps.rb") { |f| load f, true }
+
+# for CapturefulFormatter
+# https://github.com/crowdworks/captureful_formatter
+CapturefulFormatter.configure do |config|
+  config.project_name = "Test result report" # Title of test report
+  config.output_directory = "./spec/result" # The path to where the test report is saved.
+  #config.template_path = "path/to/template" # your custom template file path.
+end
